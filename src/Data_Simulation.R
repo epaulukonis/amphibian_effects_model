21ae17c5407fa8ac2d8e0ce471f9954e79c13e46
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-03-05 14:28:12 -0500

    updates to src

--- src/Data_Simulation.R
+++ src/Data_Simulation.R
@@ -23,18 +23,16 @@ set.seed(6379)
 # effects$half_life<-(effects$Duration_h*log(2))/log(1/effects$Survival)#need to modify survival by duration, using an exponential growth curve
 # effects$adj_sur_96<-round(1/(2^(96/effects$half_life)),3)
 names(effects)
-df_sig<-effects[,c(1,5,8,10,18)] #make sure this output matches
+df_sig<-effects[,c(1,5,8,10,17)] #make sure this output matches
 df_sig<-na.omit(df_sig)
 df_sig$mort<-round(df_sig$N_Exp - (df_sig$Survival*df_sig$N_Exp),0)
 df_sig$alive<-df_sig$N_Exp-df_sig$mort
 head(df_sig)
-
 #split data into list by study/species for cochran armitage trend test
 by_s_stat<-split(df_sig, list(df_sig$Study, df_sig$Species), drop=T)
 by_s_stat<-lapply(by_s_stat, function (x) x[c('Application_Rate','mort','alive')]) 
 
-
-#change format to matric to run CA trend test to test for trends in increasing significance
+#change format to matrix to run CA trend test to test for trends in increasing significance
 ch0=list()
 for (i in seq_along(by_s_stat)) {
 a=t(by_s_stat[[i]])
@@ -66,6 +64,7 @@ effects<-effects %>%
   group_by(Application_Rate)%>% #application rate
   mutate(SE = SD/sqrt(length(effects))) #mutate to add standard error to account for variability between studies
 effects<-as.data.frame(effects)
+effects<-na.omit(effects)
 controls<-effects[effects$Application_Rate == 0, ]  #take out all controls
 control<-effects[5,] # take out single row which will contain final control
 #function for weighted mean
@@ -83,7 +82,7 @@ names(effects)
 #Simulate body weights (BWs)- we need 1000 simulations of the X different BWs
 by_s<-split(effects, list(effects$Study,effects$Species,effects$Application_Rate), drop=T) #split by these groups, use SE
 bw_sim<-replicate(nsims,
-                  {normalize <- lapply(by_s, function(y) rnorm(nrow(y), mean=y[[11]], sd=y[[21]]))
+                  {normalize <- lapply(by_s, function(y) rnorm(nrow(y), mean=y[[11]], sd=y[[19]]))
                   bw_sim<-as.data.frame(unlist(normalize))
                   })
 bw_sim<-do.call(cbind.data.frame, bw_sim)
@@ -93,10 +92,11 @@ colnames(bw_sim)[1:nsims]<-names<-paste0("BW",1:nsims,"")
 survival_sim <- matrix(data=NA,nrow=nrow(effects),ncol=nsims)
 colnames(survival_sim)[1:nsims]<-paste0("Sur",1:nsims,"")
 for(i in 1:nrow(effects)){
-  survival_sim[i,]<-rbinom(nsims, effects[i,17], effects[i,20]) #trials is number of observations for each experiment, prob is adjusted prob of survival for 96 hours
+  survival_sim[i,]<-rbinom(nsims, effects[i,17], effects[i,10]) #trials is number of observations for each experiment, prob is adjusted prob of survival for 96 hours
   survival_sim[i,]<-round(survival_sim[i,]/effects[i,17],3) #divide the number surviving by total trials to find the proportion that actually survived 
 }
 survival_sim<-as.data.frame(survival_sim)
+#let's walk through this with Tom; 
 
 #simulate the exposure parameters from Purucker et al. 2021
 exposure_sims <- matrix(data=NA,nrow=nsims,ncol=6)
@@ -112,11 +112,19 @@ exposure_sims[,6]<-rnorm(nsims, mean=param[11,2], sd=param[12,2])
 exposure_sims<-as.data.frame(exposure_sims)
 
 
+#pyraclostrobin
 #calculate dermal dosage 
-pmolweight<-387.8 #g/mol #comptox
-plogKow<- 4.44 #comptox
+# pmolweight<-387.8 #g/mol #comptox
+# plogKow<- 4.44 #comptox
+# kp_pyra =  10^(-2.72+(0.71*plogKow)-(0.0061*pmolweight))
+# hl<-4.91*24 #from comptox profile
+
+#glyphosate
+#calculate dermal dosage 
+pmolweight<-169.1 #g/mol #comptox
+plogKow<- -3.12 #comptox
 kp_pyra =  10^(-2.72+(0.71*plogKow)-(0.0061*pmolweight))
-hl<-4.91*24 #from comptox profile
+hl<-4.46*24 #from comptox profile
 
 #calculate soil concentrations; application rate will remain the same, the exponent value changes with exposure parameter simulations
 soil_concs_degs<-as.data.frame(log(2)/hl*96/exposure_sims$movement_rate_mean) #use 96 as duration, as we adjusted all survivals for a 96h timeframe
@@ -148,11 +156,11 @@ exp_n<-gather(exp_n,"set","N",1:nsims)
 mort_n<-gather(mort_n,"set","Effect", 1:nsims)
 derm_dose<-gather(derm,"set","Dose",1:nsims)
 
-BMDS_Run4<-cbind(exp_n,mort_n,derm_dose) #name the output whatever run you'd like
-BMDS_Run4<-BMDS_Run4[,c(1,6,2,4)]
+BMDS_glyphosate_fin<-cbind(exp_n,mort_n,derm_dose) #name the output whatever run you'd like
+BMDS_glyphosate_fin<-BMDS_glyphosate_fin[,c(1,6,2,4)]
 
 #order by dose for clarity
-BMDS_Run4<-BMDS_Run4 %>%
+BMDS_glyphosate_fin<-BMDS_glyphosate_fin %>%
   group_by(set) %>% #set of sims
   arrange(Dose, .by_group=T) #this will rearrange the output in terms of order of sims, but will still work fine
 
@@ -175,7 +183,7 @@ BMDS_Run4<-BMDS_Run4 %>%
 # }
 
 save.image(file='myEnvironment_simulation.RData') 
-write.csv(BMDS_Run4,'data_out/BMDS_Run4.csv') #save whole file for bmds analysis
+write.csv(BMDS_glyphosate_fin,'data_out/BMDS_glyphosate_fin.csv') #save whole file for bmds analysis
 
 #scraps ###---- 
 #tests trend in proportions? in this example, there does seem to be a trend in proportions? hmm
